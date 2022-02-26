e40c195cae203751298a85c7901a4293d6ea3cf7
Arien Aliz <arien@iMac-de-Arien.local>
2022-02-25 20:54:46 -0500

    Ejercicio 9

---
+++ Ejercicio9.js
@@ -0,0 +1,19 @@
+
+var numero;
+numero = 9
+
+console.log (numero);
+
+console.log(typeof(numero));
+
+numero = '7'
+
+console.log (numero);
+
+console.log(typeof(numero));
+
+var x, y;
+
+var a = 'hola', b= 'Alba' 
+
+console.log (a, b)
