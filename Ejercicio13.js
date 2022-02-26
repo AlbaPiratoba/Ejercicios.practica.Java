bfdcd8925372c53ae9250e755ee26eaaa7d82ce9
Arien Aliz <arien@iMac-de-Arien.local>
2022-02-25 21:40:12 -0500

    Ejercicio13.js

---
+++ Ejercicio13.js
@@ -0,0 +1,17 @@
+
+// Crear una constante por medio de la Sentencia const
+
+var nombre = 'Arien';
+let lenguaje = 'Frances';
+const PI = "3.141592";
+
+nombre = 'Alba'
+
+// PI = '4' constante por lo tanto nunca permitirá su modificación
+
+const estudiante = {'id': 6, nombre: 'Arien Aliz'}
+
+console.log(estudiante)
+
+estudiante.id = 7;
+
