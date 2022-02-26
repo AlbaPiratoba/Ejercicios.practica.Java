768565825ddac00dfc09fa003e0ba70b15cfd8b7
Arien Aliz <arien@iMac-de-Arien.local>
2022-02-25 21:14:24 -0500

    Ejercicio11.js

---
+++ Ejercicio11.js
@@ -0,0 +1,24 @@
+
+
+function mostararSaludo () {
+    var mensaje = 'Hola... Este código se ejecuta en una función';
+    console.log(mensaje);
+}
+
+// console.log(mensaje);
+
+var numero = 7;
+
+function mostrarNumero () {
+    console.log ('El numero es: '+ numero);
+}
+
+mostrarNumero();
+
+var Saludo = 'Hola Alba'
+
+function mostrarSaludo2 () {
+    console.log ('Te saludamos ' + Saludo)
+}
+
+mostrarSaludo2 ();
