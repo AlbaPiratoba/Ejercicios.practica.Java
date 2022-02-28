b278321fe8e90fc57790d12ea9edf24a8c936b3e
Arien Aliz <arien@iMac-de-Arien.local>
2022-02-27 19:17:45 -0500

    ejercicio26.js

---
+++ Ejercicio15.js
@@ -0,0 +1,11 @@
+// Crear instancias de objetos por medio de constructores.
+
+new String ('JavaScript')
+
+new Array (2, 3, 4, 7, 11)
+
+let restarFn = new Function ('a', 'b', 'return a-b')
+
+persona = new Object ();
+persona.id = 1;
+persona.nombre = 'Juan';
---
+++ Ejercicio16.js
@@ -0,0 +1,16 @@
+// Ejercicio 16: Validar si una variable tiene un valor asignado
+
+let a = 'Hola'
+let b = 11;
+
+if (a === undefined) {
+    console.log ('La variable a aun no tiene un valor asignado.')
+} else { 
+    console.log ('La variable a tiene un valor asignado')
+}
+
+if (b === undefined) {
+    console.log ('La variable b aun no tiene un valor asignado.')
+} else { 
+    console.log ('La variable b tiene un valor asignado')
+}
